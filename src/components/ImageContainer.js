aba61e1c0e1ad571eaf7767efb3110c1f09788a3
andrdaun <andrdaun@gmail.com>
2023-07-05 20:24:26 +0300

    Додел блок «Car» (добавил компонент с фото и текстом на фото). Добавил информацию в блок в блоке «Work».

---
+++ src/components/ImageContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Image from "../images/imagezz.jpg"
+export const ImageContainer = () => {
+    return <div className={'imageContainer'}>
+        Это машина, на которой я катался этим летом!
+    </div>
+}
